fbbf9aa5c49a9f88e2cb2679b7c971519770c2fd
DESKTOP-2PFPM2E\TranThai <thaitran101096@gmail.com>
2019-05-16 23:09:49 +0700

    thai sua api chuyenxe get so do xe

--- APIDatVe/APIDatVe/DAL/DatVe/ChuyenXeDAL.cs
+++ APIDatVe/APIDatVe/DAL/DatVe/ChuyenXeDAL.cs
@@ -63,7 +63,7 @@ namespace APIDatVe.DAL.DatVe
 				int _seatEmpty = int.Parse(DataProvider.Instance.GetData("sp_count_seat_empty_byTripID", sqlParameter1).Rows[0]["seatEmpty"].ToString());
 				cx.soghetrong = _seatEmpty;
 				SqlParameter[] sqlParameters2 = new SqlParameter[] {
-					new SqlParameter("@maxe",cx.maxe)
+					new SqlParameter("@machuyenxe",cx.machuyenxe)
 				};
 				int _totalSeat = int.Parse(DataProvider.Instance.GetData("sp_count_car_seatnumber", sqlParameters2).Rows[0]["numberSeat"].ToString());
 				cx.tongsoghe = _totalSeat;
@@ -75,13 +75,79 @@ namespace APIDatVe.DAL.DatVe
 			return chuyenXeDTOs;
 		}
 
-		public XeDTO getSchemaCar(string carID, string tripID)
+		public XeDTO getSchemaCar1(string carID, string tripID)
 		{
 			XeDTO xeDTO = new XeDTO();
 			xeDTO.floor1 = new List<GheDTO>();
 			xeDTO.floor2 = new List<GheDTO>();
 			SqlParameter[] parameters = new SqlParameter[] {
 				new SqlParameter("@maxe",carID)
+			};
+			DataTable dt = new DataTable();
+			dt = DataProvider.Instance.GetData("sp_getSechemaCar", parameters);
+
+			//init defalut value
+			int sohang = 7;
+			int soghe = 35;
+			int socot = 5;
+			foreach (DataRow dr in dt.Rows)
+			{
+				GheDTO gheDTO = new GheDTO();
+				gheDTO.maghe = dr["maghe"].ToString();
+				gheDTO.vitriX = int.Parse(dr["vitriX"].ToString());
+				gheDTO.vitriY = int.Parse(dr["vitriY"].ToString());
+				gheDTO.tang = int.Parse(dr["tang"].ToString());
+				gheDTO.isActive = bool.Parse(dr["active"].ToString());
+				gheDTO.tenghe = dr["tenghe"].ToString();
+				gheDTO.status = 0;
+				////////////////
+				///
+
+				int.TryParse(dr["sohang"].ToString(), out sohang);
+
+				int.TryParse(dr["socot"].ToString(), out socot);
+				xeDTO.socot = socot;
+				xeDTO.sohang = sohang;
+				xeDTO.maxe = carID;
+
+				int.TryParse(dr["soghe"].ToString(), out soghe);
+				xeDTO.soghe = soghe;
+				if (gheDTO.tang == 1)
+				{
+					xeDTO.floor1.Add(gheDTO);
+				}
+				else if (gheDTO.tang == 2)
+				{
+					xeDTO.floor2.Add(gheDTO);
+				}
+			}
+			List<GheDTO> listTrangThaiGhe = getGheStatusByTripID(tripID);
+			foreach (GheDTO ghe in listTrangThaiGhe)
+			{
+				if (ghe.status == 1)
+				{
+					GheDTO ghe1 = xeDTO.floor1.SingleOrDefault(p => p.maghe == ghe.maghe);
+					GheDTO ghe2 = xeDTO.floor2.SingleOrDefault(p => p.maghe == ghe.maghe);
+					if (ghe1 != null)
+					{
+						xeDTO.floor1.Single(p => p.maghe == ghe.maghe).status = 1;
+					}
+					else if (ghe2 != null)
+					{
+						xeDTO.floor2.Single(p => p.maghe == ghe.maghe).status = 1;
+					}
+				}
+
+			}
+			return xeDTO;
+		}
+		public XeDTO getSchemaCar(string carID, string tripID)
+		{
+			XeDTO xeDTO = new XeDTO();
+			xeDTO.floor1 = new List<GheDTO>();
+			xeDTO.floor2 = new List<GheDTO>();
+			SqlParameter[] parameters = new SqlParameter[] {
+				new SqlParameter("@maxe",tripID)
 			};
 			DataTable dt = new DataTable();
 			dt = DataProvider.Instance.GetData("sp_getSechemaCar", parameters);
